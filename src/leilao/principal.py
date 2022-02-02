0c7c4dc637836c557e5d351af038d424f232bd8c
Renato Veneroso <rveneroso@gmail.com>
2022-02-02 12:21:12 -0300

    Arquivo dominio.py movido para outro pacote. Criação do arquivo principal.py no qual serão feitos os testes.

---
+++ src/leilao/principal.py
@@ -0,0 +1,15 @@
+from src.leilao.dominio import Usuario, Lance, Leilao
+
+ivan = Usuario('Ivan Piro')
+osmar = Usuario('Osmar Telo')
+
+lance_ivan = Lance(ivan, 100.0)
+lance_osmar = Lance(osmar, 150.0)
+
+leilao_celular = Leilao('Celular Moto E7 Power')
+leilao_celular.lances.append(lance_ivan)
+leilao_celular.lances.append(lance_osmar)
+
+for lance in leilao_celular.lances:
+    print(f'O usuário {lance.usuario.nome} de um lance de {lance.valor}')
+
