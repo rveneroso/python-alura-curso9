4bbab5e842615b80e08b67f366b679d0cb0c3068
Renato Veneroso <rveneroso@gmail.com>
2022-02-03 12:17:55 -0300

    Inclusão da regra de negócio que não permite que um usuário dê um lance acima do valor de sua carteira. Criação da classe de testes test_usuario.py cujos métodos foram escritos para uso com a biblioteca pytest.

---
+++ tests/test_usuario.py
@@ -0,0 +1,8 @@
+from src.leilao.dominio import Usuario, Leilao
+
+def test_deve_subtrair_valor_da_carteira_do_usuario_quando_este_propor_um_lance():
+    vini = Usuario('Vini', 100.0)
+    leilao = Leilao('Celular')
+    vini.propoe_lance(leilao, 50.0)
+
+    assert vini.carteira == 50.0
