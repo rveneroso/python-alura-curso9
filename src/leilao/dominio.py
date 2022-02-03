2c6c29eee78a5b170f4719a61ea9288c00c3883f
Renato Veneroso <rveneroso@gmail.com>
2022-02-03 09:01:09 -0300

    Remoção da classe Avaliador. Alteração da classe Leilao para que ela própria retorne o lance de menor e o lance de maior valor de uma instância. Alteração da classe de testes para refletir as demais mudanças no código.

--- src/leilao/dominio.py
+++ src/leilao/dominio.py
@@ -9,41 +9,29 @@ class Usuario:
     def nome(self):
         return self.__nome
 
-
 class Lance:
 
     def __init__(self, usuario, valor):
         self.usuario = usuario
         self.valor = valor
 
-
 class Leilao:
 
     def __init__(self, descricao):
         self.descricao = descricao
         self.__lances = []
+        self.maior_lance = sys.float_info.min
+        self.menor_lance = sys.float_info.max
 
     def registra_lance(self, lance: Lance):
         self.__lances.append(lance)
+        if (lance.valor > self.maior_lance):
+            self.maior_lance = lance.valor
+        if (lance.valor < self.menor_lance):
+            self.menor_lance = lance.valor
 
     @property
     def lances(self):
         # Com a sintaxe [:] o Python não irá retornar o objeto lances original da classe Leilao.
         # Ele irá devolver uma cópia da lista (shallow copy).
         return self.__lances[:]
-
-class Avaliador:
-
-    def __init__(self):
-        self.maior_lance = sys.float_info.min
-        self.menor_lance = sys.float_info.max
-
-    # O nome da classe Leilao no método abaixo não está limitando o parâmetro a uma instância de
-    # Leilao. É apenas uma referência para quem vier a usar essa classe saber o que passar a esse
-    # método.
-    def avalia(self,leilao: Leilao):
-        for lance in leilao.lances:
-            if (lance.valor > self.maior_lance):
-                self.maior_lance = lance.valor
-            if(lance.valor < self.menor_lance):
-                self.menor_lance = lance.valor
--- src/leilao/test_dominio.py
+++ src/leilao/test_dominio.py
@@ -1,6 +1,5 @@
 from unittest import TestCase
-from src.leilao.dominio import Usuario, Lance, Leilao, Avaliador
-
+from src.leilao.dominio import Usuario, Lance, Leilao
 
 class TestAvaliador(TestCase):
 
@@ -18,14 +17,11 @@ class TestAvaliador(TestCase):
         self.leilao.registra_lance(lance_do_yuri)
         self.leilao.registra_lance(self.lance_do_gui)
 
-        avaliador = Avaliador()
-        avaliador.avalia(self.leilao)
-
         menor_valor_esperado = 100.0
         maior_valor_esperado = 150.0
 
-        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
-        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
+        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
 
     def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_decrescente(self):
         yuri = Usuario('Yuri')
@@ -34,23 +30,17 @@ class TestAvaliador(TestCase):
         self.leilao.registra_lance(self.lance_do_gui)
         self.leilao.registra_lance(lance_do_yuri)
 
-        avaliador = Avaliador()
-        avaliador.avalia(self.leilao)
-
         menor_valor_esperado = 100.0
         maior_valor_esperado = 150.0
 
-        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
-        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
+        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
 
     def test_deve_retornar_o_mesmo_valor_para_o_maior_e_menor_lance_quando_leilao_tiver_um_lance(self):
         self.leilao.registra_lance(self.lance_do_gui)
 
-        avaliador = Avaliador()
-        avaliador.avalia(self.leilao)
-
-        self.assertEqual(150.0, avaliador.menor_lance)
-        self.assertEqual(150.0, avaliador.maior_lance)
+        self.assertEqual(150.0, self.leilao.menor_lance)
+        self.assertEqual(150.0, self.leilao.maior_lance)
 
     def test_deve_retornar_o_maior_e_o_menor_valor_quando_o_leilao_tiver_tres_lances(self):
         yuri = Usuario('Yuri')
@@ -59,17 +49,12 @@ class TestAvaliador(TestCase):
 
         lance_do_vini = Lance(vini, 200.0)
 
-        leilao = Leilao('Celular')
-
-        leilao.registra_lance(lance_do_yuri)
-        leilao.registra_lance(self.lance_do_gui)
-        leilao.registra_lance(lance_do_vini)
-
-        avaliador = Avaliador()
-        avaliador.avalia(leilao)
+        self.leilao.registra_lance(lance_do_yuri)
+        self.leilao.registra_lance(self.lance_do_gui)
+        self.leilao.registra_lance(lance_do_vini)
 
         menor_valor_esperado = 100.0
         maior_valor_esperado = 200.0
 
-        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
-        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
+        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
